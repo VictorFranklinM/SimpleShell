6dd640e64fc425575607d6dda70ce6fb25671d73
VictorFranklinM <victorfranklin2006@gmail.com>
2026-04-19 09:07:16 -0300

    feat: prompt pro usuario Lê o diretório, retorna pro usuário e lê o input

--- shell.c
+++ shell.c
@@ -1,16 +1,60 @@
 #include "shell.h"
 
+void prompt_user(void)
+{
+    char current_dir[BUFSIZ];
+
+    if (NULL == getcwd(current_dir, sizeof(current_dir)))
+        perror(RED"Não conseguiu encontrar o diretório atual"RST);
+    printf(C"%s"RST"$ ", current_dir);
+}
+
+char *shell_read_line(void)
+{
+    char *buffer = NULL;
+    size_t bufsize = 0;
+
+    prompt_user();
+
+    if (getline(&buffer, &bufsize, stdin) == -1) // Checa se houve algum erro ou se o fim do arquivo foi alcançado
+    {
+        if (feof(stdin))
+            printf(RED"[Fim do arquivo]\n"RST);
+        else
+            printf(RED"Erro no getline\n"RST);
+
+        free(buffer);
+        return NULL;
+    }
+    
+    return buffer;
+}
+
 int main(int arg_counter, char **arg_vector)
 {
-    (void)arg_counter;
-    int status;
+    char *line;
 
-    // processo filho
-    if (fork() == 0){
-        execvp(arg_vector[1], arg_vector+1);
+    // REPL
+    // Ler -> Avaliar -> Imprimir/Executar -> Loop
+
+    while (1)
+    {
+        // 1) Leitura
+        line = shell_read_line();
+
+        if(line == NULL)
+            break;
+
+        printf("%s\n", line);
+
+        free(line);
+
+        // 2) Get token
+        // Talvez trocar pra lexing e parsing
+
+        // Exec
+
     }
-
-    wait(&status);
 
     return EXIT_SUCCESS;
 }
--- shell.h
+++ shell.h
@@ -6,4 +6,21 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
+/*
+** Cores em ANSI para formatar as saídas do terminal
+**
+** Y - Amarelo
+** G - Verde
+** C - Ciano
+** RED - Vermelho
+** RST - Reset
+**
+*/
+
+#define Y "\033[1;33m"
+#define G "\033[1;32m"
+#define C "\033[1;36m"
+#define RED "\033[1;31m"
+#define RST "\033[0m"
+
 #endif
